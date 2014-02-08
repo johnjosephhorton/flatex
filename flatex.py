9f8be9a6ce3f9e80fde90309fd07cf117af73f7f
dvbrydon <david@brydon.us>
2014-02-08 13:10:35 -0600

    Update flatex.py
    Gets \input or \include.

--- flatex.py
+++ flatex.py
@@ -14,7 +14,8 @@ def is_input(line):
 uncommented out \input{} statement. Allows only spaces between 
 start of line and '\input{}'. 
     """
-    tex_input_re = r"""^\s*\\input{[^}]*}"""
+    #tex_input_re = r"""^\s*\\input{[^}]*}""" # input only 
+    tex_input_re = r"""(\\input{[^}]*})|(\\include{[^}]*})""" # input or include
     return re.search(tex_input_re, line) 
 
 def get_input(line):
